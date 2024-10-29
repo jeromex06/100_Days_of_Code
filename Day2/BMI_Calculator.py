64f01c88a52bf9598daf0981d03114db6862fbd0
jeromex06 <166141931+jeromex06@users.noreply.github.com>
2024-10-29 20:58:34 +0530

    Delete Day2 directory

--- Day2/BMI_Calculator.py
+++
@@ -1,6 +0,0 @@
-# BMI Calculator
-height = float(input("Enter your height in meters: "))
-weight = float(input("Enter your weight in kilograms: "))
-
-bmi = weight / (height ** 2)  # The BMI formula
-print(f"Your BMI is {bmi:.2f}")
